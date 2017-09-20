9625b73c3c6c43719cc4116e1ad6910a0b3c4687
Flavius Dinu <cristian@oc5487548415.ibm.com>
2017-09-20 15:40:59 +0300

    Added basic routing

--- T13 - Streams & Pipes/app.js
+++ T13 - Streams & Pipes/app.js
@@ -17,24 +17,26 @@ var fs = require('fs');
 // Server Example
 var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
-  // SENDING PLAIN TEXT
-  // res.writeHead(200, {'Content-Type': 'text/plain'});
-  // var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-  // myReadStream.pipe(res);
-
-  // SENDING HTML
-  // res.writeHead(200, {'Content-Type': 'text/html'});
-  // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  // myReadStream.pipe(res);
-
-  // SENDING JSON
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var myObj = {
-    name: 'Ryu',
-    job: 'Ninja',
-    age: 29
-  };
-  res.end(JSON.stringify(myObj));
+  if (req.url === '/home' || req.url === '/'){
+    // SENDING HTML
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
+  } else if (req.url === '/plain') {
+    // SENDING PLAIN TEXT
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    myReadStream.pipe(res);
+  } else if (req.url === '/api/ninjas'){
+    // SENDING JSON
+    var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
+  } else{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/404.html', 'utf8');
+    myReadStream.pipe(res);
+  }
 });
 
 server.listen(3000, '127.0.0.1');
